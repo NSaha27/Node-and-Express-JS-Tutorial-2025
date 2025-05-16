e9fef529cb1bfdd87756e765da7edf60cef0df24
Niladri Saha <152137415+NSaha27@users.noreply.github.com>
2025-05-16 11:09:47 +0530

    Add files via upload

---
+++ 04_stream_and-buffer/projects/proj_01_CRUD_operation_using_raw_nodejs/index.js
@@ -0,0 +1,31 @@
+import http from "http";
+
+import handleRoutes from "./routes/handleRoutes.js";
+
+const environment = {
+  staging: {
+    port: 3000,
+    env: "staging"
+  },
+  production: {
+    port: 5000,
+    env: "production"
+  }
+};
+
+const PORT = environment[process.env.NODE_ENV]["port"];
+const HOST = "127.0.0.1";
+
+
+
+const server = http.createServer((req, res) => {
+  handleRoutes.router(req, res);
+});
+
+server.listen(PORT, HOST, err => {
+  if(!err){
+    console.log(`server is running at http://${HOST}:${PORT}`);
+  }else{
+    console.error("unable to start the server!");
+  }
+})
