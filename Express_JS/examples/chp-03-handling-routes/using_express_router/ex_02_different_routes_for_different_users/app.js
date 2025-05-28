a5140d1313c83d818c16219839646c278f88042c
NSaha27 <sahaniladri27y@gmail.com>
2025-05-28 14:30:38 +0530

    different routes for different users - additing 404 not found page to this project

--- Express_JS/examples/chp-03-handling-routes/using_express_router/ex_02_different_routes_for_different_users/app.js
+++ Express_JS/examples/chp-03-handling-routes/using_express_router/ex_02_different_routes_for_different_users/app.js
@@ -2,14 +2,14 @@
 import path from "path";
 
 // external modules
-import express from "express";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
 
 // local modules
-import userRouter from "./routes/userRouter.js";
-import hostRouter from "./routes/hostRouter.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import hostRouter from "./routes/hostRouter.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 
@@ -30,7 +30,7 @@ app.use(userRouter);
 app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>404 page not found!</h1>");
+  res.status(404).render("404", {pageTitle: "404 page not found", userType: "user"});
 })
 
 app.use(errorHandler);
