c6f61c10ff50eb8b8ef3171f44bb7b060b29adac
NSaha27 <sahaniladri27y@gmail.com>
2025-05-31 14:33:57 +0530

    integrating static files and use of tailwind css in an express js project

---
+++ Express_JS/examples/chp-05-integrate_static_files/ex_01/app.js
@@ -0,0 +1,44 @@
+// core modules
+import path from "path";
+
+// external modules
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+
+// local modules
+import errorHandler from "./middlewares/errorHandler.js";
+import hostRouter from "./routes/hostRouter.js";
+import userRouter from "./routes/userRouter.js";
+
+const app = express();
+
+dotenv.config();
+
+const host = process.env.HOST || "127.0.0.1";
+const port = process.env.NODE_ENV === "development" ? 3000 : 5000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.resolve(process.cwd(), "public")));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(userRouter);
+app.use("/host", hostRouter);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {pageTitle: "404 page not found", userType: "user"});
+})
+
+app.use(errorHandler);
+
+app.listen(port, host, err => {
+  if(!err){
+    console.log(`server is running at http://${host}:${port}`);    
+  }else{
+    console.error("sorry, unable to start the server!");
+  }
+})
---
+++ Express_JS/examples/chp-05-integrate_static_files/using_tailwind_css/ex_01/routes/hostRouter.js
@@ -0,0 +1,70 @@
+import express from "express";
+
+const hostRouter = express.Router();
+
+hostRouter.get("/register", (req, res, next) => {
+  res.render("register", {pageTitle: "Host Registration", userType: "host"});
+})
+
+hostRouter.post("/register", (req, res, next) => {
+  const reqBody = req.body;
+  if(reqBody){
+    console.log(reqBody);
+    res.setHeader("status", "registration successful!");
+    res.redirect(302, "/host/login");
+  }else{
+    res.setHeader("status", "all fields are required!");
+    res.redirect(302, "/host/register");
+  }
+})
+
+hostRouter.get("/login", (req, res, next) => {
+  res.render("login", {pageTitle: "Host Login", userType: "host"});
+})
+
+hostRouter.post("/login", (req, res, next) => {
+  const reqBody = req.body;
+  if(reqBody){
+    if(reqBody.username === "NILADRISAHA" && reqBody.password === "NiladriSaha@12345"){
+      res.setHeader("status", "log in successful!");
+      res.cookie("username", reqBody.username, {maxAge: 60*60*1000, httpOnly: true});
+      res.redirect(302, "/host/home");
+    }else{
+      res.setHeader("status", "invalid log in credentials!");
+      res.redirect(302, "/host/login");
+    }
+  }else{
+    res.setHeader("status", "all fields are required!");
+    res.redirect(302, "/host/login");
+  }
+})
+
+hostRouter.get("/home", (req, res, next) => {
+  const status = req.headers["status"] ? req.headers["status"] : "";
+  const username = req.cookies["username"] ? req.cookies["username"] : "";
+  if(username){
+    res.render("host/host-home", {pageTitle: "Host home page", userType: "host", username: username, status: status});
+  }else{
+    res.setHeader("status", "please log in at first!");
+    res.redirect(302, "/host/login");
+  }
+})
+
+hostRouter.get("/add-accomodation", (req, res, next) => {
+  const username = req.cookies["username"] ? req.cookies["username"] : "";
+  res.render("host/add-accomodation", {pageTitle: "Add Accomodation", userType: "host", username: username});
+})
+
+hostRouter.post("/add-accomodation", (req, res, next) => {
+  const reqBody = req.body;
+  if(reqBody){
+    console.log(reqBody);
+    res.setHeader("status", "your accomodation details were saved successfully!");
+    res.redirect(302, "/host/add-accomodation");
+  }else{
+    res.setHeader("status", "all fields are required!");
+    res.redirect(302, "/host/add-accomodation");
+  }
+})
+
+export default hostRouter;
